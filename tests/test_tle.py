f0198ba6754873a5d7941c113d5365dc98a5b20e
Federico Stra <stra.federico@gmail.com>
2019-09-06 15:40:19 +0200

    Add more tests

--- tests/test_tle.py
+++ tests/test_tle.py
@@ -9,3 +9,9 @@ def test_from_lines(tle_lines):
 
 def test_to_orbit(tle):
     assert tle.to_orbit().ecc == tle.ecc
+
+def test_asdict(tle):
+    assert type(tle)(**tle.asdict()) == tle
+
+def test_astuple(tle):
+    assert type(tle)(*tle.astuple()) == tle
