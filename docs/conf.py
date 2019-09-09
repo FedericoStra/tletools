4f91195c3c5ae116d3d774963487edec8ed722f7
Federico Stra <stra.federico@gmail.com>
2019-09-09 15:29:11 +0200

    docs: fix version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Federico Stra'
 author = 'Federico Stra'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
