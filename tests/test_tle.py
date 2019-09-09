dba714457db8c17898e0adb9bd874a94e9d8434e
Federico Stra <stra.federico@gmail.com>
2019-09-09 18:40:58 +0200

    tests: test TLEu

--- tests/test_tle.py
+++ tests/test_tle.py
@@ -1,9 +1,15 @@
 from tletools import TLE
+from tletools.tle import TLEu
 
 
 def test_from_lines(tle_lines):
     t = TLE.from_lines(*tle_lines)
     assert isinstance(t, TLE)
+
+
+def test_from_lines_with_units(tle_lines):
+    t = TLEu.from_lines(*tle_lines)
+    assert isinstance(t, TLEu)
 
 
 def test_to_orbit(tle):
