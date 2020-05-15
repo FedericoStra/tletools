3eec084a0ff39d93d75529af1dbb65d810463ab2
Tomek Mrugalski <tomek@isc.org>
2020-05-15 02:23:23 +0200

    unit-tests extended to test M>180 deg case

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,20 @@ def tle_string():
 1 25544U 98067A   19249.04864348  .00001909  00000-0  40858-4 0  9990
 2 25544  51.6464 320.1755 0007999  10.9066  53.2893 15.50437522187805"""
 
+@pytest.fixture
+def tle_string2():
+    # This TLE tests high mean anomaly value.
+    return """NOAA 18
+1 28654U 05018A   20098.54037539  .00000075  00000-0  65128-4 0  9992
+2 28654  99.0522 154.2797 0015184  73.2195 287.0641 14.12501077766909"""
 
 @pytest.fixture
 def tle_lines(tle_string):
     return tle_string.splitlines()
 
+@pytest.fixture
+def tle_lines2(tle_string2):
+    return tle_string2.splitlines()
 
 @pytest.fixture
 def tle():
