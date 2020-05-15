6b36fa41d658f264d6283c9ec647eec3484f6052
Tomek Mrugalski <tomek@isc.org>
2020-05-15 02:23:23 +0200

    unit-tests extended to test M>180 deg case

--- tests/test_tle.py
+++ tests/test_tle.py
@@ -1,11 +1,15 @@
 from tletools import TLE
 from tletools.tle import TLEu
 
+import astropy.units as u
 
 def test_from_lines(tle_lines):
     t = TLE.from_lines(*tle_lines)
     assert isinstance(t, TLE)
 
+def test_from_lines_high_M(tle_lines2):
+    t = TLE.from_lines(*tle_lines2)
+    assert isinstance(t, TLE)
 
 def test_from_lines_with_units(tle_lines):
     t = TLEu.from_lines(*tle_lines)
@@ -14,6 +18,7 @@ def test_from_lines_with_units(tle_lines):
 
 def test_to_orbit(tle):
     assert tle.to_orbit().ecc == tle.ecc
+    assert float(tle.to_orbit().inc / u.deg) == tle.inc
 
 
 def test_asdict(tle):
